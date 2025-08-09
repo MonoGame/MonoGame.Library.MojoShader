7617a6b7bd6b716c9363aa65fd9c1ffaf26b834c
harry.cpp <harry.cpp@outlook.com>
2025-08-09 09:41:07 +0200

    This will work 2nd try

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -13,8 +13,8 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
     {
         var buildWorkingDir = "build/";
         context.ReplaceTextInFiles("mojoshader/CMakeLists.txt", "ADD_LIBRARY(mojoshader", "ADD_LIBRARY(mojoshader SHARED ");
-        context.ReplaceRegexInFiles("mojoshader/CMakeLists.txt", @"find_package\(SDL2\).+?ENDIF(SDL2_FOUND)", "");
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "CMakeLists.txt -DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" -DPROFILE_SPIRV=OFF -DPROFILE_GLSPIRV=OFF" });
+        context.ReplaceRegexInFiles("mojoshader/CMakeLists.txt", @"find_package\(SDL2\).+?ENDIF\(SDL2_FOUND\)", "");
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "../mojoshader/CMakeLists.txt -DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" -DPROFILE_SPIRV=OFF -DPROFILE_GLSPIRV=OFF" });
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build ." });
         context.CopyFile(System.IO.Path.Combine(buildWorkingDir, "libmojoshader.dylib"), $"{context.ArtifactsDir}/libmojoshader.dylib");
     }
